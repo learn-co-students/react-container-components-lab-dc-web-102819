9f2a8951c52cd646af09727c8fcb36dfa8fee7fb
Kyle <kylepcole@gmail.com>
2020-01-09 18:42:28 -0500

    styled with semantic

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
+import { Card, Header, Container } from 'semantic-ui-react'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+	constructor(){
+		super()
+		this.state = {
+			reviews: []
+		}
+	}
+
+	componentDidMount() {
+		fetch(URL)
+			.then(response => response.json())
+			.then(json => this.setState({ reviews: json.results}))
+	}
+
+	render() {
+		console.log(this.state.reviews)
+		return (
+			<Container>
+				<Header as='h2'></Header>
+			    <Header as='h1'>Recent Reviews</Header>
+				<Card.Group
+					itemsPerRow={4}
+					className="latest-movie-reviews">
+						{this.state.reviews.map(review => <MovieReviews movie={review} />)}
+				</Card.Group>
+			</Container>
+		)
+	}
+}
+
+export default LatestMovieReviewsContainer
+
+//<Card.Group centered items={items} />
+//<Card.Group itemsPerRow={4}>
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
-// Code MovieReviews Here
+import React from 'react'
+import { Card } from 'semantic-ui-react'
+
+class MovieReivews extends React.Component {
+	render() {
+		const {display_title: title, byline, summary_short, multimedia } = this.props.movie
+		return (
+			<Card key={title}
+			    image={multimedia ? multimedia.src : 'https://pmcvariety.files.wordpress.com/2013/10/film-placeholder.jpg?w=1000&h=563&crop=1'}
+			    header={title}
+			    meta={`by ${byline}`}
+			    description={summary_short}
+			/>
+		)
+	}
+}
+
+export default MovieReivews
