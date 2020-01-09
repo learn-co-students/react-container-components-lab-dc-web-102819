dccb2dc83144d75d12062b543d9905fb2d90822e
mattbechtel1 <bechma02@gmail.com>
2020-01-09 18:35:23 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,9 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = props => 
+    <ul className='review-list'>
+        {props.reviews.map(flick => <li className='review' key={flick.display_title}>{flick.display_title}: {flick.summary_short}</li>)}
+    </ul>
+
+export default MovieReviews
