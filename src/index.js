8381132df96b2e191281ae25babdfd977fc4e920
jdromero88 <github email address>
2020-01-11 11:32:06 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Container, Menu, Divider } from 'semantic-ui-react'
 
 import LatestMovieReviewsContainer from './components/LatestMovieReviewsContainer';
 import SearchableMovieReviewsContainer from './components/SearchableMovieReviewsContainer';
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
-  <div className="app">
+  <Container>
+    <Menu stackable>
+      <Menu.Item>
+        <img src='https://developer.nytimes.com/logos/logo?v=1575573724381' />
+      </Menu.Item>
+      <Menu.Item
+        name='features'>
+        NY Times
+      </Menu.Item>
+    </Menu>
     <SearchableMovieReviewsContainer />
+    <Divider />
     <LatestMovieReviewsContainer />
-  </div>,
+  </Container>,
   document.getElementById('root')
 );
